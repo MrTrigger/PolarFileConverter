8b71c623ee851b1d6138bae7506fdcba71332cff
Magnus <magnus.wallin1@gmail.com>
2014-06-27 17:37:57 +0200

    Added Polar file converter project.

---
+++ PolarFileConverter/PolarFileConverterForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace PolarFileConverter
+{
+    partial class PolarFileConverterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.selectFilesButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // selectFilesButton
+            // 
+            this.selectFilesButton.Location = new System.Drawing.Point(12, 12);
+            this.selectFilesButton.Name = "selectFilesButton";
+            this.selectFilesButton.Size = new System.Drawing.Size(130, 23);
+            this.selectFilesButton.TabIndex = 0;
+            this.selectFilesButton.Text = "Select files to convert";
+            this.selectFilesButton.UseVisualStyleBackColor = true;
+            this.selectFilesButton.Click += new System.EventHandler(this.selectFilesButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(639, 431);
+            this.Controls.Add(this.selectFilesButton);
+            this.Name = "Form1";
+            this.Text = "PolarFileConverter";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button selectFilesButton;
+    }
+}
+
---
+++ PolarFileConverter/PolarFileConverterForm.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace PolarFileConverter
+{
+    public partial class PolarFileConverterForm : Form
+    {
+        public PolarFileConverterForm()
+        {
+            InitializeComponent();
+
+            openFileDialog1.Filter = "XML Files (*.xml)|*.xml";
+            openFileDialog1.Multiselect = true;
+        }
+
+        private void selectFilesButton_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+
+            if (openFileDialog1.FileNames.Count() > 0)
+            {
+                foreach (var filename in openFileDialog1.FileNames)
+                    ConvertFile(filename);
+
+                MessageBox.Show("Finished!");
+            }
+        }
+
+        private void ConvertFile(String filename)
+        {
+            XElement xElem = XElement.Load(filename);
+
+            string hrmFilePath = Path.GetDirectoryName(filename) + "\\" + Path.GetFileNameWithoutExtension(filename) + ".hrm";
+
+            XmlReaderSettings readerSettings = new XmlReaderSettings();
+            readerSettings.IgnoreWhitespace = true;
+
+            using (System.IO.StreamWriter hrmFile = new System.IO.StreamWriter(hrmFilePath))
+            using (XmlTextReader xmlReader = new XmlTextReader(filename))
+            {
+                int interval = 0;
+
+                while (xmlReader.Read())
+                {
+                    if (xmlReader.IsStartElement())
+                    {
+                        switch (xmlReader.Name)
+                        {
+                            case "recording-rate":
+                                interval = int.Parse(xmlReader.ReadInnerXml());
+
+                                // Always use interval=5, add samples if the source file uses another interval
+                                hrmFile.WriteLine("[Params]");
+                                hrmFile.WriteLine("Interval=5");
+                                hrmFile.WriteLine();
+                                break;
+
+                            case "type":
+                                if (xmlReader.ReadInnerXml() == "HEARTRATE")
+                                {
+                                    xmlReader.ReadToFollowing("values");
+                                    string[] hrValues = xmlReader.ReadInnerXml().Split(',');
+                                    hrmFile.WriteLine("[HRData]");
+
+                                    int numSamples = interval / 5;
+                                    foreach (var v in hrValues)
+                                    {
+                                        for (int i = 0; i < numSamples; i++)
+                                            hrmFile.WriteLine(v);
+                                    }
+                                }
+                                break;
+
+                            default:
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
